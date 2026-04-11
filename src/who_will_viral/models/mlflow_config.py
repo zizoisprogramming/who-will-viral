a0f0e9f181e33a5195f4fd9bc2e9653bbb71f442
hanamostafaa <hmostafa935@gmail.com>
2026-04-11 22:18:08 +0200

    add mlruns + update ml config

--- src/who_will_viral/models/mlflow_config.py
+++ src/who_will_viral/models/mlflow_config.py
@@ -1,13 +1,12 @@
 from pathlib import Path
 import mlflow
 
-ROOT     = Path(__file__).resolve().parents[2]
-DATA_DIR = ROOT / 'data' / 'youtube'
+BASE_DIR = Path(__file__).resolve().parent  # models/
 
-MLFLOW_TRACKING_URI = (ROOT / 'mlruns').as_uri() 
-EXPERIMENT_NAME     = 'who_will_viral'
-
+MLFLOW_TRACKING_URI = (BASE_DIR / "mlruns").as_uri()
+EXPERIMENT_NAME = "youtube-viral"
 
 def setup_mlflow():
+    (BASE_DIR / "mlruns").mkdir(exist_ok=True)  # ensure folder exists
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     mlflow.set_experiment(EXPERIMENT_NAME)
