c00a2421b28a57a620e5f9a18729257222deee36
zizoisprogramming <ziadsamerbarca@gmail.com>
2026-04-15 23:15:30 +0200

    fix: adjusted the mlflow file to pass ruff checks

--- src/who_will_viral/models/mlflow_utilities.py
+++ src/who_will_viral/models/mlflow_utilities.py
@@ -1,15 +1,17 @@
 from pathlib import Path
+
+import matplotlib.pyplot as plt
 import mlflow
 from sklearn.metrics import (
-    accuracy_score, f1_score, roc_auc_score,
-
-    precision_score, recall_score,
-    classification_report, confusion_matrix,
-    ConfusionMatrixDisplay, RocCurveDisplay,
-    PrecisionRecallDisplay
+    ConfusionMatrixDisplay,
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
 )
-import matplotlib.pyplot as plt
-
 
 BASE_DIR = Path(__file__).resolve().parent  # models/
 
